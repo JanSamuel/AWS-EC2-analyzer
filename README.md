ba6772e2445a0b49de79bc524270c24fd5181f8f
Jan Owczarek <jan.owczarek@cybercom.com>
2020-06-07 22:26:14 +0200

    Creat snapshot added

--- README.md
+++ README.md
@@ -8,12 +8,19 @@ ec2-analyzer uses AWS cli configred profile to connect to EC2 instances
 To run this script aws configurtion is needed :
 'aws configure --profile ec2-analyzer'
 Script can be run with flags
- list
-	'pipenv run python ec2-analyzer.py list' - to list all EC2 instances
- start
-	'pipenv run python ec2-analyzer.py start' - to start all EC2 instances
+instances - for acess to EC2 servers
+volumes  - for acces to volumes
+snapshot - to creat and list snapshots
+additional flags are:
+ list eg.
+	'pipenv run python ec2-analyzer.py volumes list' - to list all EC2 instances
+ start eg.
+	'pipenv run python ec2-analyzer.py instances start' - to start all EC2 instances
  stop
-	'pipenv run python ec2-analyzer.py stop' - to stop all EC2 instances
+	'pipenv run python ec2-analyzer.py instances stop' - to stop all EC2 instances
+Full info can be found in :
+'pipenv run python ec2-analyzer.py --help'
+
 If script have to wokr on only specific EC2 instances with common Tag Project additional flag
 --project=projecName can be used eg.
 	'pipenv run python ec2-analyzer.py stop --project=SWA' - to stop all EC2 instances in SWA project
--- ec2-analyzer.py
+++ ec2-analyzer.py
@@ -3,6 +3,60 @@ import click
 session = boto3.Session(profile_name='ec2-analyzer')
 ec2 = session.resource('ec2')
 
+@click.group()
+def cli():
+	"EC2 manager"
+
+@cli.group('volumes')
+def volumes():
+	"Commands for volumes"
+
+@cli.group('snapshot')
+def snapshot():
+	"Commands for volumes"
+
+@snapshot.command('list')
+@click.option('--project', default=None, help="only instances for project (tag Project:<name>)")
+def snapshotList(project):
+	"list snapshots"
+	instances = getInstances(project)
+	for i in instances:
+		for v in i.volumes.all():
+			for snap in v.snapshots.all():
+				print(', '.join((
+					snap.id, snap.state, snap.progress)))
+	return
+	
+@snapshot.command('create')
+@click.option('--project', default=None, help="only instances for project (tag Project:<name>)")
+def creatSnapshot(project):
+	"createing snapshot"
+	instances = getInstances(project)
+	for i in instances:
+		for v in i.volumes.all():
+			print("Createing snapshot of {0} ...".format(v.id))
+			v.create_snapshot(Description="Created by EC2 analyzer")
+	return
+
+
+@volumes.command('list')
+@click.option('--project', default=None, help="only instances for project (tag Project:<name>)")
+def volumesList(project):
+	"list volumes"
+	instances = getInstances(project)
+	for i in instances:
+		print("EC2 instance {0} has volumes:".format(i.id))
+		for v in i.volumes.all():
+			tags = {t['Key']: t['Value'] for t in v.tags or []}
+			print('  '+', '.join((
+				v.id, v.state, str(v.size) + 'GiB', v.encrypted and "Encrypted" or "Not Encrypted")))
+	return
+
+
+@cli.group('instances')
+def instances():
+	"Commands for instances"
+
 def getInstances(project):
 	instances = []
 	if project:
@@ -11,11 +65,6 @@ def getInstances(project):
 	else:
 		instances = ec2.instances.all()
 	return instances
-
-
-@click.group()
-def instances():
-	"Commands for instances"
 
 @instances.command('start')
 @click.option('--project', default=None, help="only instances for project (tag Project:<name>)")
@@ -45,9 +94,10 @@ def listInstances(project):
 	print(', '.join(('ID','Availability Zone','State','Public DNS', 'Project')))
 	for i in instances:
 		tags = {t['Key']: t['Value'] for t in i.tags or []}
-		print(', '.join((i.id, i.instance_type, i.placement['AvailabilityZone'], i.state['Name'], i.public_dns_name, tags.get('Project','<no project>'))))
+		print(', '.join((
+			i.id, i.instance_type, i.placement['AvailabilityZone'], i.state['Name'], i.public_dns_name, tags.get('Project','<no project>'))))
 	
 if __name__=='__main__':
-	instances()
+	cli()
 
 	
