90a976146e1d88e7e6cf1dc8c215c72cd5e928d8
Jan Owczarek <jan.owczarek@cybercom.com>
2020-06-07 21:24:34 +0200

    Stop and Start EC2 functinality added

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Demo project for EC2 managment
 #configuration
 ec2-analyzer uses AWS cli configred profile to connect to EC2 instances
 
-run:
+To run this script aws configurtion is needed :
 'aws configure --profile ec2-analyzer'
-'pipenv run python ec2-analyzer.py'
+Script can be run with flags
+ list
+	'pipenv run python ec2-analyzer.py list' - to list all EC2 instances
+ start
+	'pipenv run python ec2-analyzer.py start' - to start all EC2 instances
+ stop
+	'pipenv run python ec2-analyzer.py stop' - to stop all EC2 instances
+If script have to wokr on only specific EC2 instances with common Tag Project additional flag
+--project=projecName can be used eg.
+	'pipenv run python ec2-analyzer.py stop --project=SWA' - to stop all EC2 instances in SWA project
--- ec2-analyzer.py
+++ ec2-analyzer.py
@@ -2,13 +2,52 @@ import boto3
 import click
 session = boto3.Session(profile_name='ec2-analyzer')
 ec2 = session.resource('ec2')
-@click.command()
-def listInstances():
-	print(', '.join(('ID','Availability Zone','State','Public DNS')))
-	for i in ec2.instances.all():
-		print(', '.join((i.id, i.instance_type, i.placement['AvailabilityZone'], i.state['Name'], i.public_dns_name)))
-		
-if __name__=='__main__':
 
-	listInstances()
+def getInstances(project):
+	instances = []
+	if project:
+		filters = [{'Name':'tag:Project', 'Values':[project]}]
+		instances = ec2.instances.filter(Filters=filters)
+	else:
+		instances = ec2.instances.all()
+	return instances
+
+
+@click.group()
+def instances():
+	"Commands for instances"
+
+@instances.command('start')
+@click.option('--project', default=None, help="only instances for project (tag Project:<name>)")
+def stopEC2Instance(project):
+	"Starts EC2"
+	instances = getInstances(project)
+	print(', '.join(('ID','Availability Zone','State','Public DNS', 'Project')))
+	for i in instances:
+		print("Starting {0} ...".format(i.id))
+		i.start()
+
+@instances.command('stop')
+@click.option('--project', default=None, help="only instances for project (tag Project:<name>)")
+def stopEC2Instance(project):
+	"Stops EC2"
+	instances = getInstances(project)
+	print(', '.join(('ID','Availability Zone','State','Public DNS', 'Project')))
+	for i in instances:
+		print("Stoping {0} ...".format(i.id))
+		i.stop()
+	
+@instances.command('list')
+@click.option('--project', default=None, help="only instances for project (tag Project:<name>)")
+def listInstances(project):
+	"list EC2 instances"
+	instances = getInstances(project)
+	print(', '.join(('ID','Availability Zone','State','Public DNS', 'Project')))
+	for i in instances:
+		tags = {t['Key']: t['Value'] for t in i.tags or []}
+		print(', '.join((i.id, i.instance_type, i.placement['AvailabilityZone'], i.state['Name'], i.public_dns_name, tags.get('Project','<no project>'))))
+	
+if __name__=='__main__':
+	instances()
+
 	
